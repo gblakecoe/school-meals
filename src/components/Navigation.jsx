d25ef82b790d6ece9bb751a47d2146bf8313ecc3
gblakecoe <gblakecoe@gmail.com>
2016-11-16 19:02:46 -0500

    Update Navigation.jsx
    Deleted code that displays "Data" button on navigation bar. The code that was deleted is:
    
                <NavItem eventKey={2} onClick={this.handleData}>
                <Glyphicon glyph="list-alt" />&nbsp;
                  <FormattedMessage
                      id="nav.data"
                      description="Text for the Data navigation item."
                      defaultMessage="Data"
                  />
    
                </NavItem>

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -25,6 +25,58 @@ class Navigation extends Component {
     this.props.helpData.showArticle(article)
   }
 
+//GBC. Use this code for production
+/********************/
+  render() {
+    return (
+      <Navbar fixedTop>
+        <div className="usa-grid">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <span className="hidden-xs">
+              <FormattedMessage
+                  id="nav.title"
+                  description="Text for the page title."
+                  defaultMessage="{organizationName} Application for Free and Reduced Price School Meals"
+                  values={{
+                    organizationName: organization.name
+                  }}
+              />
+            </span>
+            <span className="hidden-sm hidden-md hidden-lg">
+              <FormattedMessage
+                  id="nav.shortTitle"
+                  description="Text for the page title on mobile devices."
+                  defaultMessage="{organizationShortName} Application for School Meals"
+                  values={{
+                    organizationShortName: organization.shortname
+                  }}
+              />
+            </span>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} onClick={this.handleHelp}>
+              <Glyphicon glyph="question-sign" />&nbsp;
+              <FormattedMessage
+                  id="nav.help"
+                  description="Text for the Help navigation item."
+                  defaultMessage="Help"
+              />
+            </NavItem>
+            <LocalePicker localeData={localeData} locales={locales} />
+          </Nav>
+        </Navbar.Collapse>
+        </div>
+      </Navbar>
+    )
+  }
+/********************/
+
+//GBC. Use this code for debugging, delete or comment for production
+/********************
   render() {
     return (
       <Navbar fixedTop>
@@ -57,12 +109,13 @@ class Navigation extends Component {
         <Navbar.Collapse>
           <Nav pullRight>
             <NavItem eventKey={2} onClick={this.handleData}>
-              <Glyphicon glyph="list-alt" />&nbsp;
+            <Glyphicon glyph="list-alt" />&nbsp;
               <FormattedMessage
                   id="nav.data"
                   description="Text for the Data navigation item."
                   defaultMessage="Data"
               />
+
             </NavItem>
             <NavItem eventKey={1} onClick={this.handleHelp}>
               <Glyphicon glyph="question-sign" />&nbsp;
@@ -79,6 +132,7 @@ class Navigation extends Component {
       </Navbar>
     )
   }
+*********************/
 }
 
 Navigation.propTypes = {
